dee83d2324d6965d35d6e5a284ba3715ea25a2ce
Micheleen Harris <michhar@microsoft.com>
2019-10-14 10:08:02 -1000

    initial commit

---
+++ damage_detect/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: __init__.py
+Description: Relative imports for MaskRCNN package.
+"""
+
+from . import face
+from . import face_list
+from . import large_face_list
+from . import util
---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: setup.py
+Description: Setup script to build and distribute maskrcnn module.
+"""
+
+import io
+
+from setuptools import find_packages
+from setuptools import setup
+
+README = 'README.md'
+
+
+def readme():
+    """Parse README for long_description."""
+    return io.open(README, encoding='utf-8').read()
+
+setup(
+    name='damage_detect',
+    version='0.0.9',
+    packages=find_packages(exclude=['tests']),
+    install_requires=['requests',
+                      # Change tensorflow to tensorflow-gpu on GPU
+                      'tensorflow==1.14.0',
+                      'keras==2.2.4',
+                      'cython',
+                      'h5py==2.8.0',
+                      'scikit-image==0.15.0',
+                      'opencv-python==4.1.1.26',
+                      'matplotlib',
+                      'jupyter'],
+    author='Micheleen Harris',
+    description='Python SDK for the MaskRCNN projects',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://github.com/',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Instance Segmentation',
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'])
